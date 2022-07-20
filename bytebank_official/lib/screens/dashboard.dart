07bdf793b5ee7d823e25cfcc2aa92c2b29df996e
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2022-07-20 19:00:44 -0300

    Aula 2: Persistência de dados - Vídeo 4: InkWell no lugar do GestureDetector

--- bytebank_official/lib/screens/dashboard.dart
+++ bytebank_official/lib/screens/dashboard.dart
@@ -23,43 +23,55 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            // O "GestureDetector" é utilizado para implementar as ações de "on..." nos widgets que não possui este comportamento nativamente.
+            // O "GestureDetector" é utilizado para implementar as ações de "on..." nos widgets que não possui este
+            // comportamento nativamente.
             // O problema que o "GestureDetector" não implementa o efeito de clique no "botão".
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ContactsList(),
-                  ),
-                );
-              },
-              child: Container(
-                height: 100,
-                width: 150,
-                padding: const EdgeInsets.all(8.0),
-                // Quando usamos o "Theme.of(context)", caso o "context" utilizado seja o que está sendo recebido por parâmetro
-                // no build "ByteBankApp", estará sendo utilizada as cores do "theme" do widget "ByteBankApp", com isso o "Container"
-                // ficará com a cor azul padrão do "MaterialApp"
-                // Para utilizarmos a "primary color" que definimos no nosso "theme:", precisaremos extrair o código do nosso "home:"
-                // para um novo widget, dessa forma conseguiremos utilizar o "context" correto.
-                color: Theme.of(context).colorScheme.primary,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 32.0,
+            // child: GestureDetector(
+            // ----
+            // Para implementarmos as ações dos evento "on..." junto com o efeito de clique no botão, devemos trocar o
+            // "GestureDetector" pelo "InkWell".
+            // O "InkWell" tem o comportamento do "GestureDetector", mas sendo do próprio Material. Para conseguirmos o
+            // efeito de clique no "botão", precisamos envolver o "InkWell" pelo "Material", e também, não pode ter
+            // filhos definido sua própria cor, o "InkWell" que deve definir a cor. Por causa disso, removemos a
+            // propriedade "color:" do "Container" e colocamos a definição do "color:" no "Material".
+            child: Material(
+              color: Theme.of(context).colorScheme.primary,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ContactsList(),
                     ),
-                    Text(
-                      ContactsList.lblContacts,
-                      style: TextStyle(
+                  );
+                },
+                child: Container(
+                  height: 100,
+                  width: 150,
+                  padding: const EdgeInsets.all(8.0),
+                  // Quando usamos o "Theme.of(context)", caso o "context" utilizado seja o que está sendo recebido por parâmetro
+                  // no build "ByteBankApp", estará sendo utilizada as cores do "theme" do widget "ByteBankApp", com isso o "Container"
+                  // ficará com a cor azul padrão do "MaterialApp"
+                  // Para utilizarmos a "primary color" que definimos no nosso "theme:", precisaremos extrair o código do nosso "home:"
+                  // para um novo widget, dessa forma conseguiremos utilizar o "context" correto.
+                  // color: Theme.of(context).colorScheme.primary,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Icon(
+                        Icons.people,
                         color: Colors.white,
-                        fontSize: 16.0,
+                        size: 32.0,
                       ),
-                    ),
-                  ],
+                      Text(
+                        ContactsList.lblContacts,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
