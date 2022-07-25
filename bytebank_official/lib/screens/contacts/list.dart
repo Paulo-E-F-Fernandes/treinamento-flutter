225b7df430dd9284ef10bb9e8ec04fb48c620fcf
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2022-07-25 18:32:06 -0300

    Aula 2: Persistência de dados - Vídeo 10

--- bytebank_official/lib/screens/contacts/list.dart
+++ bytebank_official/lib/screens/contacts/list.dart
@@ -1,24 +1,63 @@
+import 'package:bytebank_official/database/app_database.dart';
 import 'package:bytebank_official/models/contact.dart';
 import 'package:bytebank_official/screens/contacts/form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
   static const String lblContacts = "Contacts";
-  final List<Contact> contacts = List.empty(growable: true);
 
-  ContactsList({Key? key}) : super(key: key);
+  const ContactsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(lblContacts)),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Contact contact = contacts[index];
-          return _ContactItem(contact);
+      // O uso do "FutureBuilder" é utilizado para atualizar ionformações de um "widget" "StatelessWidget", sem precisar
+      // alterar o nosso "widget" para "StateFull"
+      body: FutureBuilder<List<Contact>>(
+        initialData: List.empty(growable: true),
+        // Precisamos indicar qual é a execução que o "FutureBuilder" vai realizar
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              // Nesse estado, o "future" ainda não foi executado.
+              // Neste cenário, colocamos algum widget que permita que usuário clique, realize alguma ação para iniciar
+              // o "future".
+              break;
+            case ConnectionState.waiting:
+              // Nesse estado, o "future" ainda está carregando.
+              // Então, nesse estado, podemos colocar o "loading" de espera de carregamento.
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              // Nesse estado, o "snapshot" tem algum dado disponível, mas o "Future" ainda não foi finalizado.
+              // É utilizado quando temos a abordagem de carregar a tela aos poucos, por exemplo, um download.
+              break;
+            case ConnectionState.done:
+              // Nesse estado, o "Future" foi finalizado.
+              final List<Contact>? contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts![index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts?.length,
+              );
+          }
+
+          return const Text('#ERR001 - Unknown Error');
         },
-        itemCount: contacts.length,
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
