7fbbb45e36bb3910ff6c43b3e929ec3d475c7ca4
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2022-07-27 19:12:33 -0300

    Aula 2: Persistência de dados - Vídeo 12

--- bytebank_official/lib/database/app_database.dart
+++ bytebank_official/lib/database/app_database.dart
@@ -4,7 +4,29 @@ import 'package:sqflite/sqflite.dart';
 
 // Referências:
 // https://docs.flutter.dev/cookbook/persistence/sqlite
-Future<Database> createDatabase() {
+// O "async" na assinatura do método, define que utilizaremos o escopo do async/await, e ele trabalhará com o "Future"
+Future<Database> getDatabase() async {
+  // Usando o async/await
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, "bytebank.db"); // É uma boa prática colocar a extensão do arquivo como ".db"
+
+  return openDatabase(
+    path,
+    // When the database is first created, create our table.
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database.
+      db.execute("CREATE TABLE contacts(id INTEGER PRIMARY KEY, full_name TEXT, account_number INTEGER)");
+    },
+    // Set the version.
+    // This executes the onCreate function and provides a path to perform database upgrades and downgrades.
+    version: 1,
+    // o "onDowngrade" é executado quando voltamos para uma versão anterior (version:)
+    // o "onDatabaseDowngradeDelete" vai limpar os dados do database quando a versão voltar para a anterior
+    // Tomar muito cuidado ao utilizar a instrução abaixo
+    // onDowngrade: onDatabaseDowngradeDelete,
+  );
+
+  /*
   return getDatabasesPath().then((dbPath) {
     // Set the path to the database.
     // Note: Using the "join" function from the "path" package is best practice to ensure the path is correctly
@@ -27,9 +49,19 @@ Future<Database> createDatabase() {
       // onDowngrade: onDatabaseDowngradeDelete,
     );
   });
+  */
 }
 
-Future<int> save(Contact contact) {
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+
+  final Map<String, dynamic> contactMap = {}; // {} ==> Map()
+  contactMap['full_name'] = contact.fullName;
+  contactMap['account_number'] = contact.accountNumber;
+  // Retorna o "id" do registro gerado.
+  return db.insert("contacts", contactMap);
+
+  /*
   return createDatabase().then((db) {
     final Map<String, dynamic> contactMap = {}; // {} ==> Map()
     contactMap['full_name'] = contact.fullName;
@@ -37,10 +69,24 @@ Future<int> save(Contact contact) {
     // Retorna o "id" do registro gerado.
     return db.insert("contacts", contactMap);
   });
+  */
 }
 
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+
+  final List<Contact> contacts = List.empty(growable: true);
+
+  for (Map<String, dynamic> row in result) {
+    final Contact contact = Contact(row['id'], row['full_name'], row['account_number']);
+    contacts.add(contact);
+  }
+
+  return contacts;
+
+/*
+  return getDatabase().then((db) {
     return db.query('contacts').then((contactsMapList) {
       final List<Contact> contacts = List.empty(growable: true);
 
@@ -52,4 +98,5 @@ Future<List<Contact>> findAll() {
       return contacts;
     });
   });
+  */
 }
